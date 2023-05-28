6796f40cfbc2a6cd94579e1c26743be6a9945e58
blahnikkai <83978042+blahnikkai@users.noreply.github.com>
2023-05-27 22:08:31 -0400

    add google search verification and description for binomial machine

--- src/Components/ExperiencePage/ExperiencePart/index.js
+++ src/Components/ExperiencePage/ExperiencePart/index.js
@@ -2,10 +2,12 @@ export default function ExperiencePart({exp}) {
     const {img_name, alt_txt, workplace, job_title, desc} = exp
     return (
         <>
-            <img className='exp_img' src={require(`../../../images/experience_imgs/${img_name}`)} alt={alt_txt}></img>
-            <div className='exp_part'>{workplace}</div>
-            <div className='exp_part'>{job_title}</div>
-            <div className='exp_part'>{desc}</div>
+            <div>
+                <img className='exp_img' src={require(`../../../images/experience_imgs/${img_name}`)} alt={alt_txt}></img>
+            </div>
+            <div>{workplace}</div>
+            <div>{job_title}</div>
+            <div>{desc}</div>
         </>
     )
 }
--- src/Components/ExperiencePage/index.js
+++ src/Components/ExperiencePage/index.js
@@ -6,7 +6,7 @@ import './experience_page.css'
 export default function ExperiencePage() {
     return (
         <div id='exp_block'>
-            <h1 id='exp_title'>Experience</h1>
+            <h1>Experience</h1>
             <LineOrnament/>
             <div id='exp_grid'>
                 {experience_list.map((exp, ind) => <ExperiencePart key={ind} exp={exp}/>)}
