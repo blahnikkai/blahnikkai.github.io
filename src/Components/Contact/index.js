1d0bf8e104fb8136c7ce828f2be44266ece23c7d
blahnikkai <83978042+blahnikkai@users.noreply.github.com>
2024-02-05 15:34:11 -0500

    Remove mailto links from contact section

--- src/Components/Contact/index.js
+++ src/Components/Contact/index.js
@@ -8,12 +8,8 @@ export default function Contact() {
             <LineOrnament/>
             <ul className='contact_lst'>
                 <li>(786)-612-2537</li>
-                <li className='clickable_item'>
-                    <a href='mailto: blahnikkai@gmail.com'>blahnikkai@gmail.com</a>
-                </li>
-                <li className='clickable_item'>
-                    <a href='mailto: blahnikkai@ufl.edu'>blahnikkai@ufl.edu</a>
-                </li>
+                <li>blahnikkai@gmail.com</li>
+                <li>blahnikkai@ufl.edu</li>
                 <li className='clickable_item'>
                     <a href='https://www.github.com/blahnikkai'>Github</a>
                 </li>
