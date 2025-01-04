5d8577827917bbebad54b2da08122f4b5ae7b34f
blahnikkai <83978042+blahnikkai@users.noreply.github.com>
2025-01-03 21:56:37 -0500

    Add Elastic Collider Project.

--- src/Components/ShortSections/ShortProjects/ProjectPart/index.js
+++ src/Components/ShortSections/ShortProjects/ProjectPart/index.js
@@ -2,15 +2,15 @@ export default function ProjectPart({proj}) {
     const {img_name, alt_txt, link, title, tech} = proj
     return (
         <>
-            <div className='short_item'>
+            <div className='short_item short_project'>
                 <img
                     className='short_img'
                     src={require(`../../../../images/project_imgs/${img_name}`)}
                     alt={alt_txt}
                 />
             </div>
-            <a className='short_item' href={link}>{title}</a>
-            <div className='short_item'>{tech}</div>
+            <a className='short_item short_project' href={link}>{title}</a>
+            <div className='short_item short_project'>{tech}</div>
         </>
     )
 }
